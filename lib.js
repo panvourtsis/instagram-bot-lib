934218aa4b6e843781890d6e4c3eb38670d999d4
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2018-03-01 19:17:08 +0100

    [Feature] start/stop method for gui

--- lib.js
+++ lib.js
@@ -40,118 +40,125 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 const rx = require("rxjs");
-const {EventEmitter} = require("events");
+const { EventEmitter } = require("events");
 const event_emitter = new EventEmitter();
+const browser = await puppeteer.launch({ headless: config.chrome_headless, args: config.chrome_options });
+const bot = await browser.newPage();
 
 function InstagramBotLib(config) {
-    (async() => {
-        /**
-         * Init
-         * =====================
-         * Get username, password and hashtag of bot from /config.js
-         * If not exist rename config.js.tmpl to config.js and change strings
-         *
-         * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
-         * @license:    This code and contributions have 'GNU General Public License v3'
-         * @version:    0.1
-         * @changelog:  0.1 initial release
-         *              0.5 refactor: moved to puppeteer
-         *
-         */
-        const browser = await puppeteer.launch({ headless: config.chrome_headless, args: config.chrome_options });
-        const bot = await browser.newPage();
+    function start_bot() {
+        (async() => {
+            /**
+             * Init
+             * =====================
+             * Get username, password and hashtag of bot from /config.js
+             * If not exist rename config.js.tmpl to config.js and change strings
+             *
+             * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
+             * @license:    This code and contributions have 'GNU General Public License v3'
+             * @version:    0.1
+             * @changelog:  0.1 initial release
+             *              0.5 refactor: moved to puppeteer
+             *
+             */
 
-        /**
-         * Import libs
-         * =====================
-         * Modules of bot from folder ./modules
-         *
-         * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
-         * @license:    This code and contributions have 'GNU General Public License v3'
-         * @version:    0.2
-         * @changelog:  0.1 initial release
-         *              0.2 refactor: removed eval() and added require.
-         *              0.4 added: likemode_realistic
-         *
-         */
-        let utils = require(__dirname + '/modules/utils.js')(bot, config, rx ,event_emitter);
-        let login = require(__dirname + '/modules/login.js')(bot, config, utils);
-        let twofa = require(__dirname + '/modules/2FA.js')(bot, config, utils);
-        let likemode_classic = require(__dirname + '/modules/likemode_classic.js')(bot, config, utils);
-        let likemode_realistic = require(__dirname + '/modules/likemode_realistic.js')(bot, config, utils);
 
-        /**
-         * Bot variables
-         * =====================
-         * Status var if login is correct, 2FA is correct and bot start good.
-         * 1 = OK
-         * 0 = KO
-         *
-         * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
-         * @license:    This code and contributions have 'GNU General Public License v3'
-         * @version:    0.1
-         * @changelog:  0.1 initial release
-         *
-         */
-        let login_status = "";
-        let twofa_status = 1;
-        let like_status = "";
-        let pin_status = "";
+            /**
+             * Import libs
+             * =====================
+             * Modules of bot from folder ./modules
+             *
+             * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
+             * @license:    This code and contributions have 'GNU General Public License v3'
+             * @version:    0.2
+             * @changelog:  0.1 initial release
+             *              0.2 refactor: removed eval() and added require.
+             *              0.4 added: likemode_realistic
+             *
+             */
+            let utils = require(__dirname + '/modules/utils.js')(bot, config, rx, event_emitter);
+            let login = require(__dirname + '/modules/login.js')(bot, config, utils);
+            let twofa = require(__dirname + '/modules/2FA.js')(bot, config, utils);
+            let likemode_classic = require(__dirname + '/modules/likemode_classic.js')(bot, config, utils);
+            let likemode_realistic = require(__dirname + '/modules/likemode_realistic.js')(bot, config, utils);
 
-        /**
-         * Switch Mode
-         * =====================
-         * Switch social algorithms, change algorithm from config.js
-         * 
-         * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
-         * @license:    This code and contributions have 'GNU General Public License v3'
-         * @version:    0.1
-         * @changelog:  0.1 initial release
-         *
-         */
-        async function switch_mode(bot, config, utils, likemode_classic, likemode_realistic) {
-            if (config.bot_mode == "likemode_classic")
-                await likemode_classic.start(bot, config, utils);
-            else if (config.bot_mode == "likemode_realistic")
-                await likemode_realistic.start(bot, config, utils);
-        }
+            /**
+             * Bot variables
+             * =====================
+             * Status var if login is correct, 2FA is correct and bot start good.
+             * 1 = OK
+             * 0 = KO
+             *
+             * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
+             * @license:    This code and contributions have 'GNU General Public License v3'
+             * @version:    0.1
+             * @changelog:  0.1 initial release
+             *
+             */
+            let login_status = "";
+            let twofa_status = 1;
+            let like_status = "";
+            let pin_status = "";
 
-        /**
-         * Start Bot (flow) 
-         * =====================
-         * Login --> 2FA (bad location) --> 2FA (sms pin) --> social algorithm from config.js
-         *
-         * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
-         * @license:    This code and contributions have 'GNU General Public License v3'
-         * @version:    0.1
-         * @changelog:  0.1 initial release
-         *
-         */
-        login_status = await login.start(login_status);
-
-        if (login_status == 1) {
-            pin_status = await twofa.start_twofa_location_check();
-
-            if (pin_status == 0) {
-                pin_status = await twofa.start_twofa_check();
+            /**
+             * Switch Mode
+             * =====================
+             * Switch social algorithms, change algorithm from config.js
+             * 
+             * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
+             * @license:    This code and contributions have 'GNU General Public License v3'
+             * @version:    0.1
+             * @changelog:  0.1 initial release
+             *
+             */
+            async function switch_mode(bot, config, utils, likemode_classic, likemode_realistic) {
+                if (config.bot_mode == "likemode_classic")
+                    await likemode_classic.start(bot, config, utils);
+                else if (config.bot_mode == "likemode_realistic")
+                    await likemode_realistic.start(bot, config, utils);
             }
 
-            if (pin_status == 1) {
-                twofa_status = await twofa.start_twofa_location();
-            } else if (pin_status == 2) {
-                twofa_status = await twofa.start();
+            /**
+             * Start Bot (flow) 
+             * =====================
+             * Login --> 2FA (bad location) --> 2FA (sms pin) --> social algorithm from config.js
+             *
+             * @author:     Patryk Rzucidlo [@ptkdev] <support@ptkdev.io> (https://ptkdev.it)
+             * @license:    This code and contributions have 'GNU General Public License v3'
+             * @version:    0.1
+             * @changelog:  0.1 initial release
+             *
+             */
+            login_status = await login.start(login_status);
+
+            if (login_status == 1) {
+                pin_status = await twofa.start_twofa_location_check();
+
+                if (pin_status == 0) {
+                    pin_status = await twofa.start_twofa_check();
+                }
+
+                if (pin_status == 1) {
+                    twofa_status = await twofa.start_twofa_location();
+                } else if (pin_status == 2) {
+                    twofa_status = await twofa.start();
+                }
+
+                utils.logger("[INFO]", "twofa", "status " + twofa_status);
+
+                if (twofa_status >= 1) {
+                    await switch_mode(bot, config, utils, likemode_classic, likemode_realistic);
+                }
             }
 
-            utils.logger("[INFO]", "twofa", "status " + twofa_status);
+            bot.close();
 
-            if (twofa_status >= 1) {
-                await switch_mode(bot, config, utils, likemode_classic, likemode_realistic);
-            }
-        }
+        })();
+    }
 
+    function stop_bot(){
         bot.close();
-
-    })();
+    }
 }
 
 module.exports.InstagramBotLib = InstagramBotLib
